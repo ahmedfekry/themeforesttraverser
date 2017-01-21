3e01e9631aed7096017d052db361223a2caaf3ec
ahmedfekry <ahmedfikry78@gmail.com>
2017-01-21 18:53:08 +0200

    fekry getting the required attributes

--- index.php
+++ index.php
@@ -5,26 +5,44 @@ error_reporting(E_ALL);
 
 include('html_dom.php');
 
-// Retrieve the DOM from a given URL
-$html = file_get_html('https://themeforest.net/category/wordpress?page=2',false);
-
 function getPage($value='')
-{
+{	
 	$url = 'https://themeforest.net'.$value;
 	$html = file_get_html($url,false);
-	echo $html->find('.user-html ul',0)->innertext;
-}
-$i = 0;
-foreach($html->find('.product-list__columns-container') as $e) {
-	echo "<img src='".$e->getElementByTagName('img')->src ."'></img>";
-	echo $e->getElementByTagName('h3')->getElementByTagName('a')->innertext;
+	// big image
+	echo $html->find('.item-preview a img',0)->src;
 	echo "<br>";
-	echo $e->getElementByTagName('h3')->getElementByTagName('a')->href;
-	getPage($e->getElementByTagName('h3')->getElementByTagName('a')->href);
-	echo "<br><hr>";
+	// meta-attributes 
+	echo $html->find('.meta-attributes',0)->innertext;
+	echo "<br>";
+	// price 
+	echo $html->find('.js-purchase-price',0)->innertext;
+	echo "<br>";
+	// description
+	echo $html->find('.user-html',0)->innertext;
+	echo "<br>";
+}
 
+for ($i=1; $i < 60; $i++) { 
+	# code...
+	$url = 'https://themeforest.net/category/wordpress?page='.$i;
+	// Retrieve the DOM from a given URL
+	$html = file_get_html($url,false);
+
+	foreach($html->find('.product-list__columns-container') as $e) {
+		// small image
+		echo "<img src='".$e->getElementByTagName('img')->src ."'></img>";
+		echo "<br>";
+		// title
+		echo $e->getElementByTagName('h3')->getElementByTagName('a')->innertext;
+		echo "<br>";
+		// link
+		echo "<a target='_blank' href='themeforest.net/".$e->getElementByTagName('h3')->getElementByTagName('a')->href."'>link</a>";
+		echo "<br>";
+		getPage($e->getElementByTagName('h3')->getElementByTagName('a')->href);
+		echo "<br><hr>";
+
+	}
 }
     
-
-
  ?>
