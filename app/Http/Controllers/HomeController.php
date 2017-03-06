176bd3cc00ee443cf3ab48d495259216f234c5b4
ahmedfekry <ahmedfikry78@gmail.com>
2017-03-06 22:41:42 +0200

    fekry adding the home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Category;
 
 class HomeController extends Controller
 {
     public function index()
     {
-    	return view('welcome');
+    	$categories = Category::with(array('items'=>function($query){
+				        $query->limit(10);
+				    }))->get();
+
+    	// return $category;
+    	return view('welcome',compact('categories'));
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,13 @@
-<h1>Home Page</h1>
+<ul>
+    @foreach($categories as $category)
+            <li>{{$category->name}}</li>
+                <ul>
+                    @foreach($category->items as $item)
+                        <li>{{$item->title}}</li>
+                        <li>{{$item->big_picture_link}}</li>
+                        {{-- <li>=</li> --}}
+                        <br>
+                    @endforeach
+                </ul>
+    @endforeach
+</ul>
